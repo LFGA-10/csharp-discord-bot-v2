a208ad9beaf13367a30e5be7c8aa46bba3cbf633
LFGA-10 <braiselala@gmail.com>
2026-02-28 22:08:38 +0200

    Initialize csharp-discord-bot-v2

---
+++ Program.cs
@@ -0,0 +1,1 @@
+using System; class Program { static void Main() { Console.WriteLine("Starting csharp-discord-bot-v2"); } }
---
+++ README.md
@@ -0,0 +1,3 @@
+# csharp-discord-bot-v2
+
+A Discord bot written using DSharpPlus.
